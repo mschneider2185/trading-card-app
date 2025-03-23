6c08e61ae0fc76f1067f292a21c65d9c36f909d4
mschneider2185 <mike.schneider2185@gmail>
2025-03-23 02:59:55 -0400

    Fix TypeScript errors in search page and simplify component

--- src/app/search/[[...params]]/page.tsx
+++ src/app/search/[[...params]]/page.tsx
@@ -3,39 +3,11 @@ import { cookies } from 'next/headers'
 import CardGrid from '../../../components/search/CardGrid'
 import SearchFilters from '../../../components/search/SearchFilters'
 
-type SearchPageProps = {
-  params: { params: string[] }
-  searchParams: Record<string, string | string[] | undefined>
-}
-
-export default async function SearchPage(props: SearchPageProps) {
+export default async function SearchPage() {
   const supabase = createServerComponentClient({ cookies })
   
   // Build query based on search parameters
-  let query = supabase.from('cards').select('*')
-  
-  // Handle search from URL params or searchParams
-  const searchQuery = props.params.params[0] || props.searchParams?.q
-  
-  if (searchQuery) {
-    query = query.ilike('name', `%${searchQuery}%`)
-  }
-  
-  if (props.searchParams?.year) {
-    query = query.eq('year', props.searchParams.year)
-  }
-  if (props.searchParams?.set) {
-    query = query.ilike('set', `%${props.searchParams.set}%`)
-  }
-  if (props.searchParams?.condition) {
-    query = query.eq('condition', props.searchParams.condition)
-  }
-  if (props.searchParams?.minPrice) {
-    query = query.gte('price', parseFloat(props.searchParams.minPrice as string))
-  }
-  if (props.searchParams?.maxPrice) {
-    query = query.lte('price', parseFloat(props.searchParams.maxPrice as string))
-  }
+  const query = supabase.from('cards').select('*')
 
   const { data: cards, error } = await query
 
